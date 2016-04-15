a3ecdb8e0db0071356d0073cfab3507e76c162e7
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-04-15 10:27:02 +0200

    [server] example config

---
+++ server/oscar-web-config.js
@@ -0,0 +1,38 @@
+{
+	"service" : {
+		"api" : "http",
+		"port" : 8080
+	},
+	"http" : {
+		"script" : ""
+	},
+	"dbfile" :
+		{
+			"name" : "Baden Württemberg",
+			"path" : "bw",
+			"logfile": "oscarweb.log",
+			"limit" : 128, //limit the result size of old style queries
+			"chunklimit" : 8, //limit the chunk size of old style queries
+			"fullsubsetlimit" : 150, //maximum number of cells in a result for which a full subset will be created
+			"maxindexdbreq": 100, //maximum number of indexes returned per request
+			"maxitemdbreq": 100, //maximum number of returned items per request
+			"geocellcompleter" : 0, //select the geocell completer
+			"itemscompleter" : 0, //select items completer
+			"geohcompleter" : 0, //select the geo hierarchy completer
+			"geocompleter" : 0, //select geocompleter
+			"treedCQR" : true //Use treed cqr calculation instead of normal one
+		},
+	"ghfilters" :
+		[
+			{"name" : "admin_level", "k" : ["admin_level"], "kv" : {"key" : "value"}},
+			{"name" : "all" "__COMMENT" : "implicit, dont't define this"},
+			{
+				"name" : "natural_landuse",
+				"k" : ["natural", "landuse"],
+				"kv" : {
+					"admin_level" : ["1", "2", "3", "4", "5", "6"]
+				}
+			},
+			{"name" : "named", "k" : ["name"]}
+		]
+}
