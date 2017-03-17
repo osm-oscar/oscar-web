e8e8257ea629d9e467768adea1ac17ca7f718147
Daniel Bahrdt <daniel.git@funroll-loops.de>
2017-03-17 20:18:03 +0100

    [server] CQRItems: continue implementaton

--- server/CQRItems.cpp
+++ server/CQRItems.cpp
@@ -37,6 +37,8 @@ void CQRItems::all() {
 	bool withParents = sserialize::toBool(request().get("p"));
 
 	sserialize::CellQueryResult cqr;
+	sserialize::spatial::GeoHierarchySubGraph sg;
+	
 	if (m_dataPtr->ghSubSetCreators.count(regionFilter)) {
 		cqr = m_dataPtr->completer->cqrComplete(cqs, m_dataPtr->ghSubSetCreators.at(regionFilter), m_dataPtr->treedCQR);
 	}
@@ -46,15 +48,53 @@ void CQRItems::all() {
 	uint32_t itemCount = 0;
 	
 	std::ostream & out = response().out();
+	out << "[";
 	if (withParents) {
-		
+		std::unordered_set<uint32_t> tmp;
+		bool haveParents = false;
+		{
+			for(sserialize::CellQueryResult::const_iterator it(cqr.begin()), end(cqr.end()); it != end; ++it) {
+				auto cellParents = sg.cellParents(it.cellId());
+				tmp.insert(cellParents.cbegin(), cellParents.cend());
+			}
+			std::vector<uint32_t> parents(tmp.size());
+			{
+				uint32_t i = 0;
+				for(uint32_t x : tmp) {
+					parents[i] = gh.ghIdToStoreId(x);
+					++i;
+				}
+			}
+			haveParents = parents.size();
+			m_serializer.toJson(out, store.id2ItemIterator(parents.cbegin()), store.id2ItemIterator(parents.cend()), withShapes);
+		}
+		if (haveParents) { //now take care of the items
+			tmp.clear();
+			for(sserialize::CellQueryResult::const_iterator it(cqr.begin()), end(cqr.end()); it != end; ++it) {
+				for(uint32_t x : it.idx()) {
+					auto shapeType = store.geoShapeType(x);
+					if (shapeType != sserialize::spatial::GS_POINT) {
+						if (tmp.count(x)) {
+							continue;
+						}
+						tmp.insert(x);
+					}
+					auto item  = store.at(x);
+					out << ',';
+					m_serializer.toJson(out, item, withShapes);
+				}
+			}
+		}
+		else {
+			sserialize::ItemIndex itemIds = cqr.flaten();
+			m_serializer.toJson(out, store.id2ItemIterator(itemIds.begin()), store.id2ItemIterator(itemIds.end()), withShapes);
+		}
 	}
 	else {
 		sserialize::ItemIndex itemIds = cqr.flaten();
-		out << "[";
 		m_serializer.toJson(out, store.id2ItemIterator(itemIds.begin()), store.id2ItemIterator(itemIds.end()), withShapes);
-		out << "]";
 	}
+	out << "]";
 	
 	ttm.end();
 	writeLogStats("simpleCQR", cqs, ttm, cqr.cellCount(), itemCount);
