b9dea7dfbcded58642b0603b89107fadbe3f693a
Daniel Bahrdt <daniel.git@funroll-loops.de>
2018-05-07 14:27:17 +0200

    [server] KVClustering

---
+++ server/KVClustering.cpp
@@ -0,0 +1,65 @@
+#include "KVClustering.h"
+#include <cppcms/http_response.h>
+#include <cppcms/http_request.h>
+#include <cppcms/url_dispatcher.h>
+#include <cppcms/url_mapper.h>
+#include <cppcms/json.h>
+#include <cppcms/util.h>
+#include <sserialize/spatial/CellQueryResult.h>
+
+namespace oscar_web {
+
+KVClustering::KVClustering(cppcms::service& srv, const CompletionFileDataPtr& dataPtr):
+cppcms::application(srv),
+m_dataPtr(dataPtr)
+{
+	dispatcher().assign("/get", &KVClustering::get, this);
+	mapper().assign("get","/get");
+}
+
+KVClustering::~KVClustering() {}
+
+void KVClustering::get() {
+	typedef sserialize::Static::spatial::GeoHierarchy GeoHierarchy;
+	typedef liboscar::Static::OsmKeyValueObjectStore OsmKeyValueObjectStore;
+	
+	sserialize::TimeMeasurer ttm;
+	ttm.begin();
+
+	const auto & store = m_dataPtr->completer->store();
+	const auto & gh = store.geoHierarchy();
+	
+	response().set_content_header("text/json");
+	
+	//params
+	std::string cqs = request().get("q");
+	std::string regionFilter = request().get("rf");
+	std::string format = request().get("format");
+
+	sserialize::CellQueryResult cqr;
+	sserialize::spatial::GeoHierarchySubGraph sg;
+	
+	if (m_dataPtr->ghSubSetCreators.count(regionFilter)) {
+		sg = m_dataPtr->ghSubSetCreators.at(regionFilter);
+	}
+	else {
+		sg = m_dataPtr->completer->ghsg();
+	}
+	cqr = m_dataPtr->completer->cqrComplete(cqs, sg, m_dataPtr->treedCQR);
+	uint32_t itemCount = cqr.maxItems();
+	
+	
+	
+	
+	std::ostream & out = response().out();
+	
+	ttm.end();
+	writeLogStats("get", cqs, ttm, cqr.cellCount(), itemCount);
+}
+
+void KVClustering::writeLogStats(const std::string& fn, const std::string& query, const sserialize::TimeMeasurer& tm, uint32_t cqrSize, uint32_t idxSize) {
+	*(m_dataPtr->log) << "CQRItems::" << fn << ": t=" << tm.beginTime() << "s, rip=" << request().remote_addr() << ", q=[" << query << "], rs=" << cqrSize <<  " is=" << idxSize << ", ct=" << tm.elapsedMilliSeconds() << "ms" << std::endl;
+}
+
+
+}//end namespace oscar_web
---
+++ server/KVClustering.h
@@ -0,0 +1,30 @@
+#ifndef OSCAR_WEB_KV_CLUSTERING_H
+#define OSCAR_WEB_KV_CLUSTERING_H
+#include <cppcms/application.h>
+#include "types.h"
+
+namespace oscar_web {
+
+/**
+  *This is the main query completer.
+  */
+
+class KVClustering: public cppcms::application {
+public:
+	KVClustering(cppcms::service& srv, const CompletionFileDataPtr & dataPtr);
+	virtual ~CQRItems();
+	void get();
+private:
+	typedef sserialize::Static::spatial::GeoHierarchy::SubSet::NodePtr SubSetNodePtr;
+private:
+	CompletionFileDataPtr m_dataPtr;
+	liboscar::Static::OsmKeyValueObjectStore m_store;
+private:
+	void writeLogStats(const std::string & fn, const std::string& query, const sserialize::TimeMeasurer& tm, uint32_t cqrSize, uint32_t idxSize);
+};
+
+
+
+}//end namespace
+
+#endif
