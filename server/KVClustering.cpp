ef9d6735430a933822d0395065a44b994d0586cd
somakolli <st116214@stud.uni-stuttgart.de>
2018-05-23 18:21:24 +0200

    counting key value pairs

--- server/KVClustering.cpp
+++ server/KVClustering.cpp
@@ -54,16 +54,53 @@ void KVClustering::get() {
 	
 	std::ostream & out = response().out();
 
+	//count all key value pairs like described in BA_Benjamin_Kopf_2017 4.1
+	//keyMap has the key as key and an (unordered_map, int) pair as value. the unordered_pair contains the values and the count of each value. the int counts in how many items the key is present.
+	auto keyMap = std::unordered_map<std::string, std::pair<std::unordered_map<std::string, uint32_t>, uint32_t >>();
 
+	//iterate over all query result items
 	for(sserialize::CellQueryResult::const_iterator it(cqr.begin()), end(cqr.end()); it != end; ++it){
 		for(uint32_t x : it.idx()) {
 			auto item = store.at(x);
-			out << item.id() << ";";
+			//iterate over all item keys
 			for (uint32_t i = 0; i < item.size(); ++i) {
-				out << item.key(i) << "=" << item.value(i) << ";";
+			    auto keySearch = keyMap.find(item.key(i));
+			    if(keySearch == keyMap.end()){
+			        //key is not present in keyMap
+                    auto valueMap = std::unordered_map<std::string, uint32_t>();
+                    auto keyMapPair = std::make_pair(item.key(i), std::make_pair(valueMap, 1));
+                    keyMap.emplace(keyMapPair);
+			    } else {
+			        //key is present
+			        //increment key count
+                    (keySearch -> second).second++;
+			    }
+			    //fetch the updated key
+			    keySearch = keyMap.find(item.key(i));
+
+			    //try to find the value in the valueMap
+			    auto valueSearch = keySearch -> second.first.find(item.value(i));
+			    if(valueSearch != keySearch -> second.first.end()){
+			        //value found -> increment count
+			    	valueSearch -> second++;
+			    }
+			    else{
+			        //value not found -> insert it
+			    	auto valueCountPair = std::make_pair(item.value(i), 1);
+                    keySearch -> second.first.emplace(valueCountPair);
+			    }
 			}
 		}
+	}
 
+	for(auto it : keyMap){
+	    if(it.second.second > itemCount * 0.1f){
+            out << (it.first) << "{" << it.second.second << "}" << ":" << "[";
+            for(auto ite : it.second.first){
+                out << "(" << (ite.first) << "," << (ite.second) << ")";
+            }
+            out << "]";
+	    }
 	}
 
 	ttm.end();
