aea6b0ddc4141f05b33175fbc66a7823aafb4f27
Daniel Bahrdt <daniel.git@funroll-loops.de>
2018-07-16 16:08:47 +0200

    [services] make usage of gdb optional

--- service/lib/run.sh
+++ service/lib/run.sh
@@ -1,3 +1,7 @@
 cd "${OSCAR_EXEC_DIR}"
 umask u=rwx,g=rwx,o=
-sg www-data -c "cgdb --args ${OSCAR_PATH} -c ${OSCAR_CFG}"
+if [ "${USE_GDB}" = "n" ]; then
+	sg www-data -c "${OSCAR_PATH} -c ${OSCAR_CFG}"
+else
+	sg www-data -c "cgdb --args ${OSCAR_PATH} -c ${OSCAR_CFG}"
+fi
