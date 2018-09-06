988de0b647eb0390b5a3ad8699f62fb35d591920
Sokol Makolli <s.makolli@aol.de>
2018-09-06 22:59:23 +0200

    [website] parent clustering first implementation

--- website/js/kv-clustering/kv-clustering.js
+++ website/js/kv-clustering/kv-clustering.js
@@ -1,27 +1,47 @@
 define(["require", "state", "jquery", "search"],
     function (require, state, $, search) {
         var kvClustering = {
+
             fillTable: function(cqr) {
-                $.get("/oscar/kvclustering/get?q=" + cqr + "&queryId=" + state.queries.activeCqrId, function (data) {
-                    if(state.queries.activeCqrId!==data.queryId)
-                        return;
-                    const kvClusteringList = $("#kvclustering-list");
-                    kvClusteringList.empty();
-                    let liAdded = false;
-                    data.kvclustering.forEach(function(key){
-                        if(key.clValues.length > 1){
-                            kvClusteringList.append(`<li style="margin-top: 5px"><b>refine by ${key.name}(${key.count})</b></li>`);
-                            key.clValues.forEach(function(value){
-                                kvClusteringList.append(`<li class="refinement" id="@${key.name}:${value.name} ${cqr}"><a href="#">${value.name}(${value.count})</a></li>`);
-                            });
-                            liAdded = true;
+                if(!$('#clusterModi input').is(":checked")){
+                    $.get("/oscar/kvclustering/get?q=" + cqr + "&queryId=" + state.queries.activeCqrId + "&type=kv", function (data) {
+                        if(state.queries.activeCqrId!==data.queryId)
+                            return;
+                        const kvClusteringList = $("#kvclustering-list");
+                        kvClusteringList.empty();
+                        let liAdded = false;
+                        data.kvclustering.forEach(function(key){
+                            if(key.clValues.length > 1){
+                                kvClusteringList.append(`<li style="margin-top: 5px"><b>refine by ${key.name}(${key.count})</b></li>`);
+                                key.clValues.forEach(function(value){
+                                    kvClusteringList.append(`<li class="refinement" id="@${key.name}:${value.name} ${cqr}"><a href="#">${value.name}(${value.count})</a></li>`);
+                                });
+                                liAdded = true;
+                            }
+                        });
+                        if(!liAdded){
+                            kvClusteringList.append(`<li style="margin-top: 5px">no refinements for this query</li>`);
                         }
+                        kvClusteringList.removeClass("hidden");
                     });
-                    if(!liAdded){
-                        kvClusteringList.append(`<li style="margin-top: 5px">no refinements for this query</li>`);
-                    }
-                    kvClusteringList.removeClass("hidden");
-                })
+                } else {
+                    $.get("/oscar/kvclustering/get?q=" + cqr + "&queryId=" + state.queries.activeCqrId + "&type=p", function (data) {
+                        if(state.queries.activeCqrId!==data.queryId)
+                            return;
+                        const kvClusteringList = $("#kvclustering-list");
+                        kvClusteringList.empty();
+                        let liAdded = false;
+                        console.log("hallo");
+                        data.parentClustering.forEach(function(parent){
+                            liAdded = true;
+                            kvClusteringList.append(`<li class="refinement" id=""style="margin-top: 5px"><b>${parent.parentId}(${parent.itemCount})</b></li>`);
+                        });
+                        if(!liAdded){
+                            kvClusteringList.append(`<li style="margin-top: 5px">no refinements for this query</li>`);
+                        }
+                        kvClusteringList.removeClass("hidden");
+                    });
+                }
             }
         };
         return kvClustering;
--- website/js/main.js
+++ website/js/main.js
@@ -154,6 +154,11 @@ requirejs(["leaflet", "jquery", "mustache", "jqueryui", "sidebar", "mustacheLoad
                 off_label: 'Global'
             });
 
+            $("#clusterModi input").switchButton({
+                on_label: 'Parent',
+                off_label: 'Key-Value'
+            });
+
             $('#show_tree').click(function () {
                 $('#results_tree_parent').toggle();
             });
