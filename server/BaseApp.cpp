6369d219020e11c7035a354f7f8cc6a78c518d23
Daniel Bahrdt <daniel.git@funroll-loops.de>
2019-09-11 21:02:40 +0200

    [server] BaseApp class with common log functions

---
+++ server/BaseApp.cpp
@@ -0,0 +1,62 @@
+#include "BaseApp.h"
+#include <cppcms/http_request.h>
+
+namespace oscar_web {
+	
+BaseApp::BaseApp(cppcms::service& srv, CompletionFileDataPtr dataPtr, std::string const & logPrefix) :
+cppcms::application(srv),
+m_d(dataPtr),
+m_lp(logPrefix)
+{}
+
+BaseApp::~BaseApp() {}
+
+BaseApp::InternalRequestId
+BaseApp::genIntReqId(std::string const & fn) {
+	//maybe we should derive this from the request, though this should do for now
+	InternalRequestId id = ::random();
+	std::stringstream ss;
+	ss << "request=";
+	char sep = '{';
+	for(auto const & x : request().post_or_get()) {
+		ss << sep << '"' << x.first << "\":\"" << x.second << '"';
+	}
+	ss << '}';
+	log(id, fn, ss.str());
+	return id;
+}
+
+void
+BaseApp::log(std::string const & what) {
+	*(m_d->log) << what << std::endl;
+}
+
+void
+BaseApp::log(InternalRequestId intReqId, std::string const & fn, std::string const & what) {
+	std::stringstream ss;
+	ss << m_lp << "::" << fn << " t=" << std::chrono::system_clock::now().time_since_epoch().count() << "s, id=" << intReqId << ", what=" << what;
+	log(ss.str());
+}
+
+void
+BaseApp::log(InternalRequestId intReqId, std::string const & fn, sserialize::TimeMeasurer const & tm) {
+	std::stringstream ss;
+	ss << m_lp << "::" << fn << " t=" << std::chrono::system_clock::now().time_since_epoch().count() << "s, id=" << intReqId;
+	log(ss.str());
+}
+
+void
+BaseApp::log(InternalRequestId intReqId, std::string const & fn, sserialize::TimeMeasurer const & tm, sserialize::CellQueryResult const & cqr) {
+	std::stringstream ss;
+	ss << m_lp << "::" << fn << " t=" << std::chrono::system_clock::now().time_since_epoch().count() << "s, id=" << intReqId << ", rs=" << cqr.cellCount();
+	log(ss.str());
+}
+
+void
+BaseApp::log(InternalRequestId intReqId, std::string const & fn, sserialize::TimeMeasurer const & tm, sserialize::CellQueryResult const & cqr, sserialize::ItemIndex const & items) {
+	std::stringstream ss;
+	ss << m_lp << "::" << fn << " t=" << std::chrono::system_clock::now().time_since_epoch().count() << "s, id=" << intReqId << ", rs=" << cqr.cellCount() << ", is=" << items.size();
+	log(ss.str());
+}
+	
+}//end namespace oscar_web
---
+++ server/BaseApp.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cppcms/application.h>
+
+#include "types.h"
+
+namespace oscar_web {
+
+	
+class BaseApp: public cppcms::application {
+public:
+	using InternalRequestId  = uint32_t;
+public:
+	BaseApp(cppcms::service& srv, CompletionFileDataPtr dataPtr, std::string const & logPrefix);
+	~BaseApp() override;
+protected:
+	//Call this only once per request, this will log appropriate information
+	InternalRequestId genIntReqId(std::string const & fn);
+	void log(std::string const & what);
+	void log(InternalRequestId intReqId, std::string const & fn, std::string const & what);
+	void log(InternalRequestId intReqId, std::string const & fn, sserialize::TimeMeasurer const & tm);
+	void log(InternalRequestId intReqId, std::string const & fn, sserialize::TimeMeasurer const & tm, sserialize::CellQueryResult const & cqr);
+	void log(InternalRequestId intReqId, std::string const & fn, sserialize::TimeMeasurer const & tm, sserialize::CellQueryResult const & cqr, sserialize::ItemIndex const & items);
+protected:
+	inline CompletionFileDataPtr const & dptr() const { return m_d; }
+	inline CompletionFileDataPtr & dptr() { return m_d; }
+	inline oscar_web::CompletionFileData const & d() const { return *m_d; }
+	inline oscar_web::CompletionFileData & d() { return *m_d; }
+private:
+	CompletionFileDataPtr m_d;
+	std::string m_lp;
+};
+	
+}
